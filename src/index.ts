4c862d505653196ebe9571a6687f063e4806c016
MMesch <MMesch@users.noreply.github.com>
2018-07-08 11:48:42 +0200

    codemirror typings etc...

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,38 @@ import {
 
 import '../style/index.css';
 
+import CodeMirror from 'codemirror';
 
+import 'codemirror/mode/meta';
+
+import 'codemirror/mode/haskell/haskell';
+
+
+/**
+ * Define an IPython codemirror mode.
+ *
+ * It is a slightly altered Python Mode with a `?` operator.
+ */
+CodeMirror.defineMode('ihaskell', (config: CodeMirror.EditorConfiguration, modeOptions?: any) => {
+    let haskellConf: any = {};
+    for (let prop in modeOptions) {
+      if (modeOptions.hasOwnProperty(prop)) {
+        haskellConf[prop] = modeOptions[prop];
+      }
+    }
+    haskellConf.name = 'haskell';
+    haskellConf.singleOperators = new RegExp('^[\\+\\-\\*/%&|@\\^~<>!\\?]');
+    haskellConf.identifiers = new RegExp('^[_A-Za-z\u00A1-\uFFFF][_A-Za-z0-9\u00A1-\uFFFF]*');
+    return CodeMirror.getMode(config, haskellConf);
+}, 'haskell');
+
+CodeMirror.defineMIME('text/x-ipython', 'ipython');
+CodeMirror.modeInfo.push({
+  ext: [],
+  mime: 'text/x-ihaskell',
+  mode: 'ihaskell',
+  name: 'ihaskell'
+});
 /**
  * Initialization data for the ihaskell_labextension extension.
  */
