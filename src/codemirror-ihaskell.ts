761b0b92c4e3f0c62561271e5231b2985a9a1d98
MMesch <MMesch@users.noreply.github.com>
2018-07-13 12:01:52 +0200

    works

--- src/codemirror-ihaskell.ts
+++ src/codemirror-ihaskell.ts
@@ -2,21 +2,11 @@ import * as CodeMirror from 'codemirror';
 
 import 'codemirror/lib/codemirror';
 import 'codemirror/mode/haskell/haskell';
-import 'codemirror/mode/python/python';
-import 'codemirror/mode/r/r';
-import 'codemirror/mode/octave/octave';
-import 'codemirror/mode/ruby/ruby';
-import 'codemirror/mode/sas/sas';
-import 'codemirror/mode/javascript/javascript';
-import 'codemirror/mode/shell/shell';
-import 'codemirror/mode/julia/julia';
-import 'codemirror/mode/markdown/markdown';
-import 'codemirror/mode/meta';
 
 CodeMirror.defineMode("ihaskell", (config) => {
-    console.log('defining mode');
+  let hmode = CodeMirror.getMode(config, "haskell");
     return CodeMirror.multiplexingMode(
-        CodeMirror.getMode(config, "Haskell"),
+        hmode,
         {
            open: /:(?=!)/, // Matches : followed by !, but doesn't consume !
            close: /^(?!!)/, // Matches start of line not followed by !, doesn't consume character
@@ -29,7 +19,7 @@ CodeMirror.defineMode("ihaskell", (config) => {
 CodeMirror.defineMIME("text/x-ihaskell", "ihaskell");
 
 CodeMirror.modeInfo.push({
-  ext: ['ihaskell'],
+  ext: ['hs'],
   mime: "text/x-ihaskell",
   mode: 'ihaskell',
   name: 'ihaskell'
